53a8067dea112bc7ce82e8a86f79e748b66b6a74
h.kato <h.kato@cocolabo.jp>
2021-07-25 04:04:11 +0900

    GraphQLの基礎 (#1)
    * install graphql library
    
    * hello world for graphql
    
    * use argument
    
    * use object
    
    * use mutation

---
+++ server-object.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+  
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`)
+
+class RandomDie {
+  constructor (numSides) {
+    this.numSides = numSides
+  }
+
+  rollOnce () {
+    return 1 + Math.floor(Math.random() * this.numSides)
+  }
+
+  roll ({ numRolls }) {
+    let output = []
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce())
+    }
+    return output
+  }
+}
+
+const root = {
+  getDie: ({ numSides }) => {
+    return new RandomDie(numSides || 6)
+  }
+}
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
+app.listen(4000)
+
+console.log('Running a GraphQL API serve at http://localhost:4000/graphql')
